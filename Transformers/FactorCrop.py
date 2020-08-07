39d9d850a45bb88b8aeacf63515c1b127f8e10f1
AdamOlsson <Olsson.95_@hotmail.com>
2020-08-07 11:31:24 +0200

    Adds FactorCrop transformer

---
+++ Transformers/FactorCrop.py
@@ -0,0 +1,28 @@
+import numpy as np
+import cv2
+
+class FactorCrop(object):
+
+    def __init__(self, factor, dest_size=368):
+        self.factor = factor
+        self.dest_size = dest_size
+
+    def __call__(self, sample):
+        image, label = sample['image'], sample['label']
+        
+        min_dimension = np.min(image.shape[:2])
+        
+        scale_factor = float(self.dest_size) / min_dimension
+
+        image_resized = cv2.resize(image, None, fx=scale_factor, fy=scale_factor)
+
+        h, w, c = image_resized.shape
+
+        # TODO: find out the purpose of this
+        h_new = int(np.ceil( h / self.factor))*self.factor
+        w_new = int(np.ceil( w / self.factor))*self.factor
+
+        image_cropped = np.zeros([h_new, w_new, c], dtype=image.dtype)
+        image_cropped[:h, :w, :] = image_resized
+
+        return {'image': image_cropped, 'label':label}
