644d051dbf973c03e0a95cca51cc20d76f386c7e
AdamOlsson <Olsson.95_@hotmail.com>
2020-08-09 17:52:33 +0200

    First attempt to make it a package

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name='PosePrediction',
+      version='1.0',
+      description='Human pose prediction using graphs',
+      author='Adam Olsson',
+      #author_email='',
+      #url='https://www.python.org/sigs/distutils-sig/',
+      packages=['Transformers', 'paf'],
+     )
