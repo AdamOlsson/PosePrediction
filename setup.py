9e693f902aa09c847ec15d2d01a17c8bf5e913ed
AdamOlsson <Olsson.95_@hotmail.com>
2020-08-09 18:11:39 +0200

    Creates a package

--- setup.py
+++ setup.py
@@ -1,9 +1,30 @@
 from distutils.core import setup
+
+install_requires = [
+    "torch",
+    "torchvision",
+    "pandas",
+    "opencv-python",
+    "numpy",
+    "matplotlib"
+]
+
+packages = [
+    "Transformers",
+    "paf",
+    "paf.pafprocess",
+    "Datasets",
+    "model",
+    "model.FeatureExtractors",
+    "util"
+]
+
 setup(name='PosePrediction',
       version='1.0',
       description='Human pose prediction using graphs',
       author='Adam Olsson',
       #author_email='',
       #url='https://www.python.org/sigs/distutils-sig/',
-      packages=['Transformers', 'paf', 'paf.pafprocess']
+      install_requires=install_requires,
+      packages=packages,
      )
