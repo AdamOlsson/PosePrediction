9c9b54a95ac6f47fb80d2af46fd10de94634ea58
AdamOlsson <Olsson.95_@hotmail.com>
2020-08-10 14:31:20 +0200

    Adds VideoDataset

---
+++ Datasets/VideoDataset.py
@@ -0,0 +1,36 @@
+import pandas as pd # easy load of csv
+import cv2
+import numpy as np
+
+import torch, torchvision
+from torch.utils.data import Dataset
+
+
+class VideoDataset(Dataset):
+    def __init__(self, path_csv, path_root, transform=None):
+        self._path_csv  = path_csv
+        self._path_root = path_root
+        self.annotations = pd.read_csv(path_csv)
+        self.transform = transform
+
+
+    def __len__(self):
+        return len(self.annotations)
+
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+
+        vid_name = self._path_root + self.annotations.iloc[idx,0]
+
+        vframes, _, _ = torchvision.io.read_video(vid_name) # Tensor[T, H, W, C]) – the T video frames
+        label = self.annotations.iloc[idx,1]
+
+        vfames = vframes.numpy()
+
+        sample = {'data':vframes, 'label':label, 'copy':np.copy(vframes), 'name':vid_name, 'type':'video'}
+
+        if self.transform:
+            sample = self.transform(sample)
+        
+        return sample
