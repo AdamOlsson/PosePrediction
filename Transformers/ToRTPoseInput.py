e561aecf779ac5c183d05336954ff3610de62ed9
AdamOlsson <Olsson.95_@hotmail.com>
2020-08-10 14:45:34 +0200

    Transformers now work with video

--- Transformers/ToRTPoseInput.py
+++ Transformers/ToRTPoseInput.py
@@ -3,13 +3,15 @@ import numpy as np
 
 class ToRTPoseInput(object):
 
-    def __init__(self, dim):
+    def __init__(self, dim=0):
         self.dim = dim
 
     def __call__(self, sample):
         
-        data = sample['data']
-        data = np.expand_dims(data, self.dim)
+        data, type = sample['data'], sample['type']
+        
+        if type == 'image': # Input to network is 4 dimensional
+            data = np.expand_dims(data, self.dim)
 
         sample['data'] = torch.from_numpy(data).float()
 
