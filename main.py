236c0fa8c8240ce54e300fb38b27e50fa081eef6
AdamOlsson <Olsson.95_@hotmail.com>
2020-08-11 14:45:32 +0200

    General improvements

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ from Transformers.ToRTPoseInput import ToRTPoseInput
 
 # util
 from util.load_config import load_config
-from util.load_save_humans import save_humans
+from paf.util import save_humans, load_humans
 from paf.paf_to_pose import paf_to_pose_cpp
 from paf.common import draw_humans, CocoPart
 from paf.body_part_construction import body_part_construction, body_part_translation
@@ -24,25 +24,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 import cv2
 
-
-def showRandomSample(dataset):
-    """
-    Sample and display 4 random samples from the dataset.
-    """
-    fig, ax = plt.subplots(1,4, figsize=(15,8))
- 
-    for i in range(4):
-        rnd = np.random.randint(len(dataset))
-        item = dataset[rnd]
-        image, label = item['image'], item['label']
-
-        if isinstance(item['image'], torch.Tensor):
-            image = image.numpy().transpose((0, 2 ,3 ,1))
-        
-        ax[i].imshow(image[0,:])
-        ax[i].set_title(item['label'], fontsize=20, fontweight='bold') 
- 
-    plt.show()
 
 if __name__ == "__main__":
 
@@ -55,17 +36,16 @@ if __name__ == "__main__":
     config = load_config("config.json")
 
     no = 1
+    fs = 8
     transformers = [FactorCrop(config["model"]["downsample"], dest_size=config["dataset"]["image_size"]), RTPosePreprocessing(), ToRTPoseInput(0)]
-    image_dataset = ImageDataset(image_path_annotations, image_path_data, transform=Compose(transformers))
-    video_dataset = VideoDataset(video_path_annotations, video_path_data, transform=Compose(transformers))
+    image_dataset = ImageDataset(image_path_annotations, image_path_data, transform=Compose(transformers), load_copy=True)
+    video_dataset = VideoDataset(video_path_annotations, video_path_data, transform=Compose(transformers), load_copy=False, frame_skip=fs)
     
-    dataset = video_dataset
-    #dataset = image_dataset
+    #dataset = video_dataset
+    dataset = image_dataset
 
     data = dataset[no]['data']
     data_copy = dataset[no]['copy']
-
-    #showRandomSample(dataset)
 
     model = PoseModel()
     model.load_state_dict(torch.load("model/weights/vgg19.pth"))
@@ -76,23 +56,19 @@ if __name__ == "__main__":
     paf = branch1.data.numpy().transpose(0, 2, 3, 1)
     heatmap = branch2.data.numpy().transpose(0, 2, 3, 1)
 
-    #humans = paf_to_pose_cpp(heatmap, paf, config)
-
-    no_frames = 2#len(paf[:]) # == len(heatmap[:])
+    no_frames = len(paf[:]) # == len(heatmap[:])
     frames = []
     for frame in range(no_frames):
         humans = paf_to_pose_cpp(heatmap[frame], paf[frame], config)
         frames.append(humans)
     
-    metadata = {"filename": dataset[no]['name'], "body_part_translation":body_part_translation, "body_construction":body_part_construction}
+    metadata = {"filename": dataset[no]['name'], "body_part_translation":body_part_translation, "body_construction":body_part_construction, "frame_skip":fs}
+    
+    save_humans("data/humans_image.json", frames, metadata)
 
-    save_humans(frames, metadata)
-    out0 = draw_humans(data_copy[0], frames[0])
-    out1 = draw_humans(data_copy[1], frames[1])
+    out = draw_humans(data_copy, frames[0])
 
-    cv2.imwrite('result0.png', out0)
-    cv2.imwrite('result1.png', out1)
-
+    cv2.imwrite('result.png', out)
 
     # TODO: Look at Dawids Thesis
     # TODO: Do pose prediction for video
