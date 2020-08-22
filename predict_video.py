4ce38763311f0c644e1fbdd6bf91323ed87dc64f
AdamOlsson <olsson.95_@hotmail.com>
2020-08-22 16:49:31 +0200

    Small fies

--- predict_video.py
+++ predict_video.py
@@ -41,9 +41,6 @@ if __name__ == "__main__":
 
     device = "cuda"
 
-    image_path_data = "data/images/"
-    image_path_annotations = "data/images/annotations.csv"
-
     video_path_data = "data/videos/"
     video_path_annotations = "data/videos/annotations.csv"
 
@@ -51,23 +48,21 @@ if __name__ == "__main__":
 
     no = 0
     fs = 1
+
     transformers = [FactorCrop(config["model"]["downsample"], dest_size=config["dataset"]["image_size"]), RTPosePreprocessing(), ToRTPoseInput(0)]
-    #image_dataset = ImageDataset(image_path_annotations, image_path_data, transform=Compose(transformers), load_copy=True)
     video_dataset = VideoDataset(video_path_annotations, video_path_data, transform=Compose(transformers), load_copy=False, frame_skip=fs)
 
-    #dataset = video_dataset
+    sample = video_dataset[no]
 
     model = PoseModel()
     model = model.to(device)
     model.load_state_dict(torch.load("model/weights/vgg19.pth", map_location=torch.device(device)))
-
+ 
     video_predictor = VideoPredictor(model, video_dataset, 64, device, output_handler=output_handler)
 
     with torch.no_grad():
         (branch1, branch2) = video_predictor.predict(no)
 
-    # branch1.shape = torch.Size([1, 38, 82, 46])
-    # branch2.shape = torch.Size([1, 19, 82, 46])
 
     paf = branch1.data.cpu().numpy().transpose(0, 2, 3, 1)
     heatmap = branch2.data.cpu().numpy().transpose(0, 2, 3, 1)
@@ -78,19 +73,17 @@ if __name__ == "__main__":
         humans = paf_to_pose_cpp(heatmap[frame], paf[frame], config)
         frames.append(humans)
 
-    save_file = "data/graphs/{}/humans_{}.json".format(video_dataset[no]['type'], video_dataset[no]['type'])
-    metadata = {"filename": video_dataset[no]['name'], "body_part_translation":body_part_translation, "body_construction":body_part_construction, "frame_skip":fs, "label":video_dataset[no]['label'], "info":video_dataset[no]['info']}
+    save_file = "data/graphs/{}/humans_{}.json".format(sample['type'], sample['type'])
+    metadata = {
+        "filename": sample['name'],
+        "body_part_translation":body_part_translation,
+        "body_construction":body_part_construction,
+        "frame_skip":fs,
+        "label":sample['label'],
+        "info":sample['info']
+    }
+
     save_humans(save_file, frames, metadata)
     print(save_file)
 
-    # for images only
-    #out = draw_humans(data_copy, frames[0])
-    #cv2.imwrite('results/result.png', out)
-
-    # TODO: Look at Dawids Thesis
-    # TODO: When the graphs are connected temporally, I might need to add a 3d dimension. Preferebly this would
-    #       be time in seconds. Is there metadata about a video so I can get the recorded fps and this the time 
-    #       between frames?
     # TODO: Remove background humans
-    # TODO: Build a graph between frames in poses
-    # TODO: Verify that downloading this package and using in another package works as intended, i.e imports.
